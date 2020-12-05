fb70acb3114a185c2b4328fbed558023bc0cd455
duywoo <tangtheduy@gmail.com>
2020-12-05 12:46:04 +0700

    app update lan 4

--- app/src/main/java/com/example/application/object/WritingComment.java
+++ app/src/main/java/com/example/application/object/WritingComment.java
@@ -1,10 +1,14 @@
 package com.example.application.object;
 
+import java.util.ArrayList;
+
 /*public class WritingComment extends BaseComment {
     private String writingId;
 
     private ArrayList<ReplyComment> replies;
 } */
 public class WritingComment{
+    private String writingId;
 
+    private ArrayList<ReplyComment> replies;
 }
