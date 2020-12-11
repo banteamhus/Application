7a141d8ffcc7cd171617db9e91f8e1d782616f40
banteamhus <73920333+banteamhus@users.noreply.github.com>
2020-12-11 13:33:32 +0700

    update

--- app/src/main/java/com/example/application/object/Comment.java
+++ app/src/main/java/com/example/application/object/Comment.java
@@ -5,8 +5,36 @@ public class Comment {
     private int type; //1 = writing - 2 reply
     private String userId;
     private String commentId;
-
-
-
-
+    public Comment (){
+    }
+    public Comment (String rId, String uId, String cId, int type){
+        rootId = rId;
+        this.type = type;
+        userId = uId;
+        commentId = cId;
+    }
+    public void setRootId (String rId){
+        rootId = rId;
+    }
+    public void setUserId (String uId){
+        userId = uId;
+    }
+    public void setCommentId (String cId){
+        commentId = cId;
+    }
+    public void isReply (int type){
+        this.type = type;
+    }
+    public String getRootId (){
+        return rootId;
+    }
+    public String getUserId (){
+        return userId;
+    }
+    public String getCommentId (){
+        return commentId;
+    }
+    public int getType (){
+        return this.type;
+    }
 }
